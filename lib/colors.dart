00889647d0c019fb526fbe6f36ed65f95ecf05b9
Hemish Pancholi <hemishpancholi11@gmail.com>
2021-02-06 23:39:16 +0530

    Added colors

---
+++ lib/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static Color primaryColor = Color(0xFF87C71A);
+  static Color accentColor = Color(0xFFDFF6B7);
+  static Color backgroundColor = Color(0xFFFFFFFF);
+
+  static Color fontColor = Color(0xFF000000);
+  static Color transparent = Color(0x00FFFFFF);
+}
